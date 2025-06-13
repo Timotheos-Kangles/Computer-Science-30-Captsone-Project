048a19816f3412fe877319b5aebc70a016a67065
Shaheer2007 <shaheer.arif@rbe.sk.ca>
2025-06-12 20:59:51 -0600

    forgot to remove the debug print, removed it now

--- Game/Modules/Main_Game/game_objects.py
+++ Game/Modules/Main_Game/game_objects.py
@@ -1,20 +1,12 @@
 from Game.Classes.planet_class import Planet
 from Game.Classes.player_class import Player_obj
 
-# global variables
-
-
 # Initialize game objects
 earth_obj = Planet("Earth")
 mars_obj = Planet("Mars")
 player = Player_obj()
-
 # earth specific objects
 earth_grid1, vis_grid = earth_obj.create_grid(3, 3)
-
 # mars specific objects
 mars_grid1, vis_grid = mars_obj.create_grid(4, 4)
-
-
-print("DEBUG: mars_obj =", mars_obj)
 
